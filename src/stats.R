4e95e7a0f1b314018b0a66ee1217517130fe16a2
Kaede Ito <mapleia01@gmail.com>
2023-12-04 01:56:33 -0800

    Added boxplot + line graph combo

--- src/stats.R
+++ src/stats.R
@@ -9,10 +9,11 @@ sheet_url = "https://docs.google.com/spreadsheets/d/1bcj83_IeYjC3aGNcZdNVh8MFgFJ
 data_df_raw <- read_sheet(sheet_url,
                           sheet="simplified weight data",
                           range="A1:E20"
-  ) %>%
+  )
+
+loss_df <- data_df_raw %>%
   mutate(tape_type = factor(tape_type,
-                            levels=c("scotch", "masking", "packing"),
-                            ordered=TRUE)
+                            levels=c("scotch", "masking", "packing"))
          ) %>%
   mutate(
     weight_loss_0_1 = 100.0,
@@ -24,43 +25,43 @@ data_df_raw <- read_sheet(sheet_url,
          weight_loss_0_1,weight_loss_1_2,
          weight_loss_2_3,weight_loss_3_4
          ) %>%
-  mutate(
-    id = seq.int(nrow(data_df_raw))
-  )
-
-levels(data_df_raw$tape_type)
-
-loss_df <- data_df_raw %>%
   pivot_longer(cols=weight_loss_0_1:weight_loss_3_4,
-                        names_to = "loss_day",
-                        values_to = "percent_loss",
-                        names_prefix = "weight_loss_"
+               names_to = "loss_day",
+               values_to = "percent_loss",
+               names_prefix = "weight_loss_"
   ) %>%
   mutate(loss_day = factor(loss_day,
                            levels=c("0_1", "1_2", "2_3", "3_4"),
-                           ordered = TRUE)
+                           ordered = TRUE),
+         id = row_number()
          )
-levels(loss_df$loss_day)
+
+# get outliers
+outliers_df <- loss_df %>%
+  group_by(tape_type, loss_day) %>%
+  identify_outliers(percent_loss)
 
 without_outliers <- loss_df %>%
-  filter(loss_day != "0_1") %>%
+  filter(loss_day != "0_1") %>% 
   group_by(tape_type, loss_day) %>%
   filter(!is_outlier(percent_loss)) %>%
   ungroup()
-
+  
 # get summary statistics (sd and mean)
 without_outliers %>%
   group_by(tape_type, loss_day) %>%
   get_summary_stats(percent_loss, type = "mean_sd")
 
-loss_df %>%
-  group_by(tape_type, loss_day) %>%
-  identify_outliers(percent_loss)
+is.factor(without_outliers$tape_type)
+is.factor(without_outliers$loss_day)
 
-# Two-way mixed ANOVA test
-res.aov <- without_outliers %>% 
-  anova_test(dv = percent_loss, wid = id,
-             between = tape_type, within = loss_day)
+unique(without_outliers$tape_type)
+unique(without_outliers$loss_day)
+
+which(sapply(without_outliers, function(x) (is.character(x) | is.factor(x)) & length(unique(x))<2))
+
+# Two-way ANOVA test
+res.aov <- loss_df %>% anova_test(percent_loss ~ tape_type * loss_day)
 get_anova_table(res.aov)
 
 # Effect of group at each time point
@@ -79,29 +80,26 @@ one.way2 <- without_outliers %>%
   adjust_pvalue(method = "bonferroni")
 one.way2
 
+pwc <- without_outliers %>%
+  group_by(loss_day) %>%
+  pairwise_t_test(percent_loss ~ tape_type, p.adjust.method = "bonferroni")%>% 
+  add_xy_position(x = "loss_day")
+pwc
 
-
-
-create_plot <- function(no_outliers_df, full_df) {
+create_box_plot <- function(no_outliers_df, full_df, pwc) {
   # Pairwise comparisons between group levels
-  pwc <- no_outliers_df %>%
-    group_by(loss_day) %>%
-    pairwise_t_test(percent_loss ~ tape_type, p.adjust.method = "bonferroni")
-  pwc
-  
-  pwc <- pwc %>% add_xy_position(x = "loss_day")
-  loss_label <- c("Original weight", "Loss on day 2", "Loss on day 3", "Loss on day 4")
+  pwc.filtered <- pwc %>% filter(loss_day != "0_1")
+  loss_label <- c("1", "2", "3", "4")
   bxp <- ggboxplot(
     full_df, x = "loss_day", y = "percent_loss",
-    color = "tape_type", palette = "jco"
-  ) + 
-    stat_pvalue_manual(pwc, tip.length = 0, hide.ns = TRUE) +
+    color = "tape_type", palette = "jco") + 
+    stat_pvalue_manual(pwc.filtered, tip.length = 0, hide.ns = TRUE) +
     labs(
-      subtitle = get_test_label(res.aov, detailed = TRUE),
-      caption = get_pwc_label(pwc),
-      title="Weight loss",
+      # subtitle = get_test_label(res.aov, detailed = TRUE),
+      # caption = get_pwc_label(pwc),
+      title="Figure 2a. Average Weight Loss",
       x = "Day of observation",
-      y = "Average of original weight (%)",
+      y = "Percent of original weight (%)",
       colour = "Tape types"
     ) +
     scale_x_discrete(labels=loss_label)
@@ -118,4 +116,29 @@ create_plot <- function(no_outliers_df, full_df) {
 }
 
 
-bxp <- create_plot(without_outliers, loss_df)
+bxp <- create_box_plot(without_outliers, loss_df, pwc)
+bxp
+
+nocommon_legend <- ggarrange(bxp,lnp,ncol=1, nrow=2, common.legend = FALSE, legend="bottom")
+common_legend <- ggarrange(bxp,lnp,ncol=1, nrow=2, common.legend = TRUE, legend="bottom")
+
+width <- 11.2
+height <- 14
+
+ggsave("outputs/fig_2_no_common_legend.png",
+       nocommon_legend,
+       device="png",
+       units="cm",
+       width=width,
+       height=height,
+       dpi = 200,
+)
+
+ggsave("outputs/fig_2_common_legend.png",
+       common_legend,
+       device="png",
+       units="cm",
+       width=width,
+       height=height,
+       dpi = 200,
+)
--- src/survivability.R
+++ src/survivability.R
@@ -2,10 +2,12 @@ library(tidyverse)
 library(googlesheets4)
 library(readxl)
 library(tidyverse)
+library(ggpubr)
+
 sheet_url = "https://docs.google.com/spreadsheets/d/1bcj83_IeYjC3aGNcZdNVh8MFgFJbSqJEdrvB0ZNb7bc/edit?usp=sharing"
 survivability_raw_df <- read_sheet(sheet_url,
                           sheet="hh_data")%>%
-  mutate_at(vars(TAPE), factor)
+  mutate(TAPE = factor(TAPE, levels=c("scotch", "masking", "packing")))
 
 percent_surv_df <- survivability_raw_df %>%
   group_by(TAPE, DAY) %>%
@@ -13,17 +15,30 @@ percent_surv_df <- survivability_raw_df %>%
             count_total = length(SURVIVED)) %>%
   mutate(percent_survived = count_survived/count_total * 100)
 
-glimpse(percent_surv_df)
-percent_surv_df %>%
-  ggplot(
-    aes(x=DAY, y=percent_survived,
-        group=TAPE,
-        colour=TAPE
-        )
-    ) +
-  geom_line() +
+create_survivability_plot <- function(dat_df) {
+  lnp <- ggline(
+    data=percent_surv_df,
+    x="DAY",
+    y="percent_survived",
+    color="TAPE",
+    palette = "jco"
+  ) +
   labs(
-    title="Survivability of different tapes",
+    title="Figure 2b. Survivability of different tapes",
     x = "Day of observation",
-    y = "Embryos that survived (%)"
+    y = "Embryos that survived (%)",
+    color="Tape types"
   )
+  png_path <- file.path("outputs", "survivability_plot.png")
+  ggsave(png_path,
+         lnp,
+         device="png",
+         units="cm",
+         width=15,
+         height=9,
+         dpi = 200,
+  )
+  return(lnp)
+}
+
+lnp <- create_survivability_plot(percent_surv_df)
